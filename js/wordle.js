6285b3328c04193d884e3144ff2179c5f8ad6658
Melwiin <melwin.kramer@gmail.com>
2022-02-25 17:29:41 +0100

    core wordle

--- js/wordle.js
+++ js/wordle.js
@@ -1,19 +1,65 @@
 var wordle_grid = document.getElementById("wordle-grid");
+
+var curr_row = 0;
+
 var letters = 5;
 var rows = 6;
 
-for (let r = 0; r < rows; r++) 
-{
-    var n_row = document.createElement("div");
-    n_row.className = "row";
+var input = "";
 
-    for (let l = 0; l < letters; l++) 
+initialize();
+
+function initialize() {
+    input = "";
+    curr_row = 0;
+
+    wordle_grid.innerHTML = "";
+
+    for (let r = 0; r < rows; r++) 
     {
-        var n_letter = document.createElement("div");
-        n_letter.className = "input";
+        var n_row = document.createElement("div");
+        n_row.className = "row";
+    
+        for (let l = 0; l < letters; l++) 
+        {
+            var n_letter = document.createElement("div");
+            n_letter.className = "input";
+    
+            n_row.appendChild(n_letter);
+        }
+    
+        wordle_grid.appendChild(n_row);
+    }
+}
 
-        n_row.appendChild(n_letter);
+function updateInput() {
+    for(let i = 0; i < letters; i++) {
+        if(input[i] == undefined){
+            wordle_grid.children.item(curr_row).children.item(i).innerHTML = "";
+        }else{
+            wordle_grid.children.item(curr_row).children.item(i).innerHTML = input[i];
+        }
+    }
+}
+
+document.body.addEventListener("keydown", (e) => {
+    if(input.length < letters){
+        if(isLetter(e.key)) {
+            input+=e.key.toUpperCase();
+            updateInput();
+        }
+    }else if(e.key == "Enter") {
+        curr_row++;
+        input = "";
     }
 
-    wordle_grid.appendChild(n_row);
+    if(e.key == "Backspace") {
+        input = input.substring(0, input.length - 1);
+        updateInput();
+    }
+});
+
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
 }
