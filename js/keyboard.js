4063f372f6b214dd222795b29808e3f9a9339e53
Macbook <macbook@MacBook-Air-von-Timo.local>
2022-02-25 18:02:45 +0100

    dwad

---
+++ js/keyboard.js
@@ -0,0 +1,41 @@
+var v_keyboard = document.getElementById("v-keyboard");
+var label_list = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Z",
+  "U",
+  "I",
+  "O",
+  "P",
+  "Ü",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "Ö",
+  "Ä",
+  "Löschen",
+  "Y",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "Enter",
+];
+v_keyboard.appendChild(document.createElement("button"));
+
+/*
+for (let r = 0; r < label_list.length; r++) {
+    
+}
+*/
