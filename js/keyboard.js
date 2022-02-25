be6360c215db8847f2dd4dee0e7fa9223d9a0ffd
Macbook <macbook@MacBook-Air-von-Timo.local>
2022-02-25 19:14:23 +0100

    bruh

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,41 +1,18 @@
 var v_keyboard = document.getElementById("v-keyboard");
-var label_list = [
-  "Q",
-  "W",
-  "E",
-  "R",
-  "T",
-  "Z",
-  "U",
-  "I",
-  "O",
-  "P",
-  "Ü",
-  "A",
-  "S",
-  "D",
-  "F",
-  "G",
-  "H",
-  "J",
-  "K",
-  "L",
-  "Ö",
-  "Ä",
-  "Löschen",
-  "Y",
-  "X",
-  "C",
-  "V",
-  "B",
-  "N",
-  "M",
-  "Enter",
-];
-v_keyboard.appendChild(document.createElement("button"));
+var liste = ["Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P", "Ü", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ö", "Ä", "Delete", "Y", "X", "C", "V", "B", "N", "M", "Enter"]
 
-/*
-for (let r = 0; r < label_list.length; r++) {
-    
+for (let r = 0; r < liste.length; r++) {
+
+  var button_name = document.createElement("button");
+  button_name.innerHTML = liste[r];
+
+  if (button_name.innerHTML == "Delete") {
+    button_name.id = "delete"
+  }
+  if (button_name.innerHTML == "Enter") {
+    button_name.id = "enter"
+  }
+
+  v_keyboard.appendChild(button_name);
 }
-*/
+
