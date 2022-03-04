04b75267f7211fcf7c24d81a54ecaed21e3cbdea
Melwiin <melwin.kramer@gmail.com>
2022-03-04 15:14:11 +0100

    aufräumen comments optimisierung

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,38 +1,28 @@
 var v_keyboard = document.getElementById("v-keyboard");
-var liste = ["Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P", "Ü", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ö", "Ä", "Delete", "Y", "X", "C", "V", "B", "N", "M", "Enter"]
+var key_list = ["Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P", "Ü", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ö", "Ä", "Delete", "Y", "X", "C", "V", "B", "N", "M", "Enter"]
 
-for (let r = 0; r < liste.length; r++) {
+//Init call -> possible reinit of keyboard (clear keyboard)
+initKeyboard();
+function initKeyboard() {
+  //loop trough list 
+  key_list.forEach(key => {
+    var btn = document.createElement("button");
+    btn.innerHTML = key;
+    btn.type = "button";
 
-  var button_name = document.createElement("button");
-  button_name.innerHTML = liste[r];
+    //exceptions for enter and delete
+    if (btn.innerHTML == "Delete") btn.id = "delete";
+    else if (btn.innerHTML == "Enter") btn.id = "enter";
+    // button_name.class = "button" <--- keine ahnung was das macht?
 
-  if (button_name.innerHTML == "Delete") {
-    button_name.id = "delete"
+    v_keyboard.appendChild(btn);
+  });
+
+
+  // bt = document.getElementById("v-keyboard").children; || Why getelementbyid again? 
+  var bt = v_keyboard.children;
+  for (var i = 0; i < bt.length; i++) {
+    /*EVENT LISTENER*/
+    bt[i].addEventListener("click", (e) => pressKey(e.target.innerHTML));
   }
-  else if (button_name.innerHTML == "Enter") {
-    button_name.id = "enter"
-  }
-  button_name.class = "button"
-
-  v_keyboard.appendChild(button_name);
 }
-
-
-function getKeyFromButton(button){
-  //button.
-}
-
-/*EVENT LISTENER*/
-bt = document.getElementById("v-keyboard").children;
-
-
-for(var i = 0; i<bt.length; i++){
-  bt[i].addEventListener("click", (e)=> pressKey(e.target.innerHTML));
-}
-
-  //element.addEventListener("click", pressKey(element.innerHTML))
-
-
-
-
-
